231fe238d888c20b06f7082abce2079fd6ca7adf
Hugo Saladini <hg.saladini@gmail.com>
2025-03-13 13:28:19 -0300

    feat(card): create model card and annotation

---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,27 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(scale = 13, precision = 2)
+    private Double limit;
+
+    public Long getId() { return id; }
+    public void setId( Long id) { this.id = id; }
+
+    public String getNumber() { return number; }
+    public void setNumber( String number ) { this.number = number; }
+
+    public Double getLimit() { return limit; }
+    public void setLimit( Double limit ) { this.limit = limit; }
+
+}
